752347211bd45f381ce2add8dd30e207de37a093
edwcored <edwin.cortes@carvajal.com>
2019-10-22 11:27:23 -0500

    archivos base
    se agregan archivos base

---
+++ server.js
@@ -0,0 +1,64 @@
+//Initiallising node modules
+var express = require("express");
+var cors = require('cors')
+var bodyParser = require("body-parser");
+var app = express();
+const path = require('path');
+var compression = require('compression');
+var helper = require("./models/helperMongo");
+
+const CONFIG = require("./utils/Config");
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+//CORS Middleware
+app.use(cors());
+app.use(compression()); //Compress all routes
+
+//connect to mongo
+helper.initPool();
+
+//OTERS Middleware
+app.use(function (req, res, next) {
+    res.header("Cache-Control", "no-cache");
+    res.header("Cache-Control", "no-store");
+    //TokenUtils.validateToken(req, res, next);
+    next();
+});
+
+
+const persona = require('./routes/persona');
+app.use('/api/persona', persona);
+
+const producto = require('./routes/producto');
+app.use('/api/productos', producto);
+
+const dominiovalor = require('./routes/dominiovalor');
+app.use('/api/dominiovalor', dominiovalor);
+
+const geografia = require('./routes/geografia');
+app.use('/api/geografia', geografia);
+
+const utilidades = require('./routes/utilidades');
+app.use('/api/utilidades', utilidades);
+
+const fe = require('./routes/facturaElectronica');
+app.use('/api/fe', fe);
+
+
+app.use('/images', express.static(CONFIG.RUTAIMG));
+
+/*
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist/frontend')));
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/frontend/index.html'));
+});
+*/
+//Setting up server
+var server = app.listen(82, function () {
+    var port = server.address().port;
+    console.log("REST funcionando el puerto: ", port);
+});
+
